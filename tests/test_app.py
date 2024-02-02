3247f9bc0ea075bcd0986c2cc4fd7ff97cdd3757
magno lacen <magnoodilon60@gmail.com>
2024-02-02 16:57:52 -0300

    REFACTORE: Refaturando nosso teste

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,23 +1,43 @@
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
 from time import sleep
+import pytest
+import subprocess
+
+@pytest.fixture
+def driver():
+    # Iniciar o Streamlit em background
+    process = subprocess.Popen(["streamlit", "run", "src/app.py"])
+
+    # inicair o Webdriver Usando o GeckoDriver
+    driver = webdriver.Chrome()
+    driver.set_page_load_timeout(5)
+    yield driver
+
+    # Fechar o WebDriver e o Streamlit após o teste
+    driver.quit()
+    process.kill()
 
 
-# Precisamos definir qual driver vamos utilizar
-driver = webdriver.Chrome()
-
-# definir um timeout implicito
-driver.set_page_load_timeout(5) # 5 segundos
-
-# vamos fazer uma tratativa de try-except de entrar na nossa página
-try:
+def test_app_opens(driver):
+    # Verificar se a página abre
     driver.get("http://localhost:8501")
     sleep(5)
-    print("Acessou a página com sucesso")
-except TimeoutException:
-    print("Tempo de carregamento da página excedeu o limite")
-finally:
-    driver.quit()
+
+# # Precisamos definir qual driver vamos utilizar
+# driver = webdriver.Chrome()
+
+# # definir um timeout implicito
+# driver.set_page_load_timeout(5) # 5 segundos
+
+# # vamos fazer uma tratativa de try-except de entrar na nossa página
+# try:
+#     driver.get("http://localhost:8501")
+#     sleep(5)
+#     print("Acessou a página com sucesso")
+# except TimeoutException:
+#     print("Tempo de carregamento da página excedeu o limite")
+# finally:
+#     driver.quit()
 
 
 
